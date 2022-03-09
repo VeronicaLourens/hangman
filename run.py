6ce60443de650883550ea340ab12cd51e4a33292
Veronica Lourens <Veronica@lourens.cloud>
2022-03-09 19:50:06 +0000

    Add get random color and ask user to play or not functions.

--- run.py
+++ run.py
@@ -33,12 +33,35 @@ def get_random_color():
     The function is to get random color.
     """
 
+    color = random.choice(colors)
+    print(color)
+
 
 def ask_user_to_play_or_not():
     """
     The function is to let user decide 
     if he/she plays the game or not.
+    print out the messages base on user's answer.
     """
+
+    while True:
+
+        user_choice = input('Would you like to play Hangman game? ')
+        print('\n')
+
+        if user_choice == 'yes':
+            print("Great! Let's go ahead and play.")
+            print()
+
+        elif user_choice == 'no':
+            print('No worries! Until next time.')
+            print()
+            quit()
+
+        else:
+            print('Invalid data.')
+            print("Please enter 'yes' or 'no' next time.")
+            print()
 
 
 def get_user_name():
@@ -67,3 +90,7 @@ def main():
     """
     The function is to call all of the functions.
     """
+    get_random_color()
+    ask_user_to_play_or_not()
+
+main()
