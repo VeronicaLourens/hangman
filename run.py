c21997103a2f2486143e1abb036dd2cba77eaed2
Veronica Lourens <Veronica@lourens.cloud>
2022-03-09 19:31:21 +0000

    Add basic function structure with docstrings, import random module and a list of colors.

--- run.py
+++ run.py
@@ -1,3 +1,69 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+# Hangman game is to guess a color.
+# User makes a guess by guessing a letter each time.
+# The color is chosen by the computer randomly.
+
+
+# A list of colors
+
+colors = [
+    'aqua',
+    'blue',
+    'green',
+    'yellow',
+    'orange',
+    'red',
+    'white',
+    'violet',
+    'indigo',
+    'brown',
+    'black',
+    'navy',
+    'purple',
+    'rose',
+    'gray',
+    'silver',
+    'orchid',
+    'golden'
+]
+
+def get_random_color():
+    """
+    The function is to get random color.
+    """
+
+
+def ask_user_to_play_or_not():
+    """
+    The function is to let user decide 
+    if he/she plays the game or not.
+    """
+
+
+def get_user_name():
+    """
+    The function is to get user's name.
+    Display welcoming message to the user.
+    """
+
+
+def play_game():
+    """
+    The function is to play the game.
+    User guess a color by enter a letter each time.
+
+    """
+
+
+def hangman_image():
+    """
+    The function is to print out the Hangman body parts
+    on each time user guesses a wrong letter.
+    """
+
+
+def main():
+    """
+    The function is to call all of the functions.
+    """
