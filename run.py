6bcadbce06a51b0d3b6b9d8c8a3e4955b16df202
Veronica Lourens <Veronica@lourens.cloud>
2022-03-10 10:57:10 +0000

    Add validate user answer and play game functions.

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ def get_random_number():
     """
     The function is to get random number.
     """
-    random_number = random.randint(1, 50)
+    random_number = random.randint(0, 50)
     print(random_number)
 
 
@@ -28,6 +28,7 @@ def ask_user_to_play_or_not():
         if user_choice.lower() == 'yes':
             print("Great! Let's go ahead and play.")
             print()
+            break
             
 
         elif user_choice.lower() == 'no':
@@ -39,6 +40,8 @@ def ask_user_to_play_or_not():
             print('Invalid data.')
             print("Please enter 'yes' or 'no' next time.")
             print()
+            continue
+            
 
 
 def get_user_name():
@@ -55,10 +58,39 @@ def get_user_name():
 
             print()
             print('Nice to meet you  ' + user_name + '!')
-            
+
+            break
 
 
-get_user_name()
+# get_user_name()
+
+def validate_user_answer():
+    """
+    The function is to validate user's answer,
+    to see if user enters a digit.
+    """
+
+    while True:
+        # try:
+        #     user_answer = input('Please guess a number between 0 and 50: ')
+
+        # except ValueError():
+        #     print('Invalid data. Please enter a number: ')
+       
+
+        if user_answer.isdigit():
+            user_answer = int(user_answer)
+
+        # elif user_answer <= 0:
+        #     print('Please enter a number bigger than 0: ')
+
+        else: 
+            print('Please enter only numbers next time.')
+            print()
+
+# validate_user_answer()
+
+
             
 
 def play_game():
@@ -69,6 +101,30 @@ def play_game():
 
     """
 
+    attempts = 0
+    max_attempts = 10
+
+    answer = random.randint(0, 50)
+    user_answer = input('Please guess a number: ')
+
+    while user_answer != answer:
+        if user_answer > answer:
+            print('Your number is too big. Please try again!')
+
+        elif user_answer < answer:
+            print('Your number is too small. Please try again!')
+
+
+
+play_game()
+
+
+
+
+    
+
+
+# play_game()
 
 def hangman_image():
     """
@@ -81,8 +137,9 @@ def main():
     """
     The function is to call all of the functions.
     """
-    get_random_number()
+    answer = get_random_number()
     ask_user_to_play_or_not()
-    get_user_name()
+    username = get_user_name()
+    user_answer = validate_user_answer()
 
 # main()
